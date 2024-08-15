b891de072219d482cc5aaf4815cd2adb2dc365d0
LuMariGames <kai.hono426@gmail.com>
2024-08-15 10:37:06 +0900

    Add files via upload

---
+++ source/main.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+
+void draw_debug(float x, float y, const char *text);
+bool get_isPause();
+void debug_touch(int x, int y);
+bool get_isMusicStart();
+char* get_buffer();
+int powi(int x, int y);
+int pause_window(touchPosition tp, unsigned int key);
+int message_window(touchPosition tp, unsigned int key,int text);
+int exist_file(const char* path);
+
+#define BUFFER_SIZE 160 //バッファ用文字列のサイズ
