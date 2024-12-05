c5ac7e0c69f05c4d9eb2f0ceb3a9c5bb84ea7212
LuMariGames <kai.hono426@gmail.com>
2024-12-05 12:58:26 +0900

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -332,9 +332,9 @@ int main() {
 			}
 			draw_score(sprites);
 			if (ndspChnIsPlaying(CHANNEL) == true) {
-				std::string floatString = std::to_string(myFloat);
+				std::string floatString = std::to_string(ndspChnGetRate(CHANNEL));
 				const char* myString = floatString.c_str();
-				draw_debug(0, 0, ndspChnGetRate(myString));
+				draw_debug(0, 0, myString);
 			}
 
 			C2D_TargetClear(bottom, C2D_Color32(0xFF, 0xE7, 0x8C, 0xFF));	//下画面
