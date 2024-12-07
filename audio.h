f7920d333628c5ec12eccce181a514b51fe2e950
LuMariGames <kai.hono426@gmail.com>
2024-12-07 19:33:15 +0900

    Add files via upload

--- audio.h
+++ audio.h
@@ -3,7 +3,7 @@ void load_sound();
 void sd_load_sound();
 void sd_load_combo();
 int play_sound(int id);
-int play_combo();
+int play_combo(int id);
 void exit_music();
 int music_SamplePos(int id);
 int ogg_check();
