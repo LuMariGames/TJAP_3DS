a3e1be9008a5a6f90deb8a43aabf328185ab9fdf
LuMariGames <kai.hono426@gmail.com>
2024-12-07 16:32:56 +0900

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -250,10 +250,10 @@ int main() {
 			draw_title();
 			draw_emblem(sprites);
 			if (course == COURSE_DAN) draw_condition();
-			if (ndspChnIsPlaying(CHANNEL) == true) {
+/*			if (ndspChnIsPlaying(CHANNEL) == true) {
 				snprintf(get_buffer(), BUFFER_SIZE, "%.8f", ndspChnGetRate(CHANNEL));
 				draw_debug(0, 0, get_buffer());
-			}
+			}*/
 
 			if (cnt == 0) {
 				FirstMeasureTime = get_FirstMeasureTime();
