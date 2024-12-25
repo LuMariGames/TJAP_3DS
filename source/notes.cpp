517b0d52049a014c8d75a011a43de7b2343057c8
LuMariGames <kai.hono426@gmail.com>
2024-12-25 20:11:17 +0900

    Add files via upload

--- source/notes.cpp
+++ source/notes.cpp
@@ -678,7 +678,7 @@ void notes_calc(bool isDon, bool isKatsu, double bpm, double CurrentTimeNotes, i
 
 		if (Notes[i].flag == true) {
 
-			Notes[i].x = Notes[i].x_ini - NOTES_AREA * Notes[i].scroll * (CurrentTimeNotes - (Notes[i].pop_time - (240.0/Notes[i].bpm*(Notes[i].bpm/(NowBPM*4))))) / (240.0/Notes[i].bpm);
+			Notes[i].x = Notes[i].x_ini - NOTES_AREA * Notes[i].scroll * (CurrentTimeNotes - (Notes[i].pop_time - (240.0/Notes[i].bpm*((Notes[i].bpm/NowBPM)-1)))) / (240.0/Notes[i].bpm);
 
 			switch (Notes[i].knd) {
 
