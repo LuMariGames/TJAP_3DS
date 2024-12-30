5cef4e8f7df663fee7c6e983949e59b584372c2f
LuMariGames <kai.hono426@gmail.com>
2024-12-30 11:25:52 +0900

    Update notes.cpp

--- source/notes.cpp
+++ source/notes.cpp
@@ -347,7 +347,7 @@ void notes_main(bool isDon, bool isKatsu, char tja_notes[MEASURE_MAX][NOTES_MEAS
 	draw_emblem(sprites);
 	draw_judge(CurrentTimeNotes, sprites);
 
-	int id = MeasureCount - 1;
+	int id = MeasureCount - 3;
 
 	if (Measure[id].judge_time+240.0/Measure[id].bpm*Measure[id].measure <= CurrentTimeNotes) NowBPM = Measure[id].bpm;
 	
