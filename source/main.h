446eb4d68a9445911847697158aa8b8a049308df
LuMariGames <kai.hono426@gmail.com>
2025-01-10 08:39:37 +0900

    Update main.h

--- source/main.h
+++ source/main.h
@@ -10,6 +10,5 @@ int pause_window(touchPosition tp, unsigned int key);
 int message_window(touchPosition tp, unsigned int key,int text);
 int exist_file(const char* path);
 int time_count(double TIME);
-double get_offset(TJA_HEADER_T TJA_Header);
 
 #define BUFFER_SIZE 160 //バッファ用文字列のサイズ
