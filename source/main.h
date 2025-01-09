5cbc83e2140438db3cc81864e6eb1cc3d57c6fc6
LuMariGames <kai.hono426@gmail.com>
2025-01-10 08:24:57 +0900

    Update main.h

--- source/main.h
+++ source/main.h
@@ -10,5 +10,6 @@ int pause_window(touchPosition tp, unsigned int key);
 int message_window(touchPosition tp, unsigned int key,int text);
 int exist_file(const char* path);
 int time_count(double TIME);
+double get_offset();
 
 #define BUFFER_SIZE 160 //バッファ用文字列のサイズ
