07b41a22b84924b2c2fb353d3de84e326b347eac
LuMariGames <kai.hono426@gmail.com>
2025-01-18 21:05:29 +0900

    Update playback.cpp

--- source/playback.cpp
+++ source/playback.cpp
@@ -99,11 +99,7 @@ void playFile(void* infoIn){
 	int16_t*	buffer2 = NULL;
 	int16_t*	buffer3 = NULL;
 	int16_t*	buffer4 = NULL;
-	int16_t*	buffer5 = NULL;
-	int16_t*	buffer6 = NULL;
-	int16_t*	buffer7 = NULL;
-	int16_t*	buffer8 = NULL;
-	ndspWaveBuf		waveBuf[8];
+	ndspWaveBuf		waveBuf[4];
 	bool			lastbuf = false;
 	int				ret = -1;
 	const char*		file = info->file;
@@ -143,10 +139,6 @@ void playFile(void* infoIn){
 	buffer2 = (int16_t*)linearAlloc(decoder.vorbis_buffer_size * sizeof(int16_t));
 	buffer3 = (int16_t*)linearAlloc(decoder.vorbis_buffer_size * sizeof(int16_t));
 	buffer4 = (int16_t*)linearAlloc(decoder.vorbis_buffer_size * sizeof(int16_t));
-	buffer5 = (int16_t*)linearAlloc(decoder.vorbis_buffer_size * sizeof(int16_t));
-	buffer6 = (int16_t*)linearAlloc(decoder.vorbis_buffer_size * sizeof(int16_t));
-	buffer7 = (int16_t*)linearAlloc(decoder.vorbis_buffer_size * sizeof(int16_t));
-	buffer8 = (int16_t*)linearAlloc(decoder.vorbis_buffer_size * sizeof(int16_t));
 
 	ndspChnReset(CHANNEL);
 	ndspChnWaveBufClear(CHANNEL);
@@ -177,22 +169,6 @@ void playFile(void* infoIn){
 	waveBuf[3].nsamples = (*decoder.decode)(&buffer4[0]) / (*decoder.channels)();
 	waveBuf[3].data_vaddr = &buffer4[0];
 	ndspChnWaveBufAdd(CHANNEL, &waveBuf[3]);
-
-	waveBuf[4].nsamples = (*decoder.decode)(&buffer5[0]) / (*decoder.channels)();
-	waveBuf[4].data_vaddr = &buffer5[0];
-	ndspChnWaveBufAdd(CHANNEL, &waveBuf[4]);
-
-	waveBuf[5].nsamples = (*decoder.decode)(&buffer6[0]) / (*decoder.channels)();
-	waveBuf[5].data_vaddr = &buffer6[0];
-	ndspChnWaveBufAdd(CHANNEL, &waveBuf[5]);
-	
-	waveBuf[6].nsamples = (*decoder.decode)(&buffer7[0]) / (*decoder.channels)();
-	waveBuf[6].data_vaddr = &buffer7[0];
-	ndspChnWaveBufAdd(CHANNEL, &waveBuf[6]);
-
-	waveBuf[7].nsamples = (*decoder.decode)(&buffer8[0]) / (*decoder.channels)();
-	waveBuf[7].data_vaddr = &buffer8[0];
-	ndspChnWaveBufAdd(CHANNEL, &waveBuf[7]);
 	
 	while(ndspChnIsPlaying(CHANNEL) == false);
 
@@ -202,11 +178,7 @@ void playFile(void* infoIn){
 		if(lastbuf == true && waveBuf[0].status == NDSP_WBUF_DONE &&
 			waveBuf[1].status == NDSP_WBUF_DONE &&
 			waveBuf[2].status == NDSP_WBUF_DONE &&
-			waveBuf[3].status == NDSP_WBUF_DONE &&
-			waveBuf[4].status == NDSP_WBUF_DONE &&
-			waveBuf[5].status == NDSP_WBUF_DONE &&
-			waveBuf[6].status == NDSP_WBUF_DONE &&
-			waveBuf[7].status == NDSP_WBUF_DONE)
+			waveBuf[3].status == NDSP_WBUF_DONE)
 			break;
 
 		if(ndspChnIsPaused(CHANNEL) == true || lastbuf == true)
@@ -271,66 +243,6 @@ void playFile(void* infoIn){
 
 			ndspChnWaveBufAdd(CHANNEL, &waveBuf[3]);
 		}
-
-		if(waveBuf[4].status == NDSP_WBUF_DONE){
-
-			size_t read = (*decoder.decode)(&buffer5[0]);
-
-			if(read <= 0)
-			{
-				lastbuf = true;
-				continue;
-			}
-			else if(read < decoder.vorbis_buffer_size)
-				waveBuf[4].nsamples = read / (*decoder.channels)();
-
-			ndspChnWaveBufAdd(CHANNEL, &waveBuf[4]);
-		}
-
-		if(waveBuf[5].status == NDSP_WBUF_DONE)
-		{
-			size_t read = (*decoder.decode)(&buffer6[0]);
-
-			if(read <= 0)
-			{
-				lastbuf = true;
-				continue;
-			}
-			else if(read < decoder.vorbis_buffer_size)
-				waveBuf[5].nsamples = read / (*decoder.channels)();
-
-			ndspChnWaveBufAdd(CHANNEL, &waveBuf[5]);
-		}
-
-		if(waveBuf[6].status == NDSP_WBUF_DONE)
-		{
-			size_t read = (*decoder.decode)(&buffer7[0]);
-
-			if(read <= 0)
-			{
-				lastbuf = true;
-				continue;
-			}
-			else if(read < decoder.vorbis_buffer_size)
-				waveBuf[6].nsamples = read / (*decoder.channels)();
-
-			ndspChnWaveBufAdd(CHANNEL, &waveBuf[6]);
-		}
-
-		if(waveBuf[7].status == NDSP_WBUF_DONE)
-		{
-			size_t read = (*decoder.decode)(&buffer8[0]);
-
-			if(read <= 0)
-			{
-				lastbuf = true;
-				continue;
-			}
-			else if(read < decoder.vorbis_buffer_size)
-				waveBuf[7].nsamples = read / (*decoder.channels)();
-
-			ndspChnWaveBufAdd(CHANNEL, &waveBuf[7]);
-		}
 	}
 
 	(*decoder.exit)();
@@ -346,10 +258,6 @@ out:
 	linearFree(buffer2);
 	linearFree(buffer3);
 	linearFree(buffer4);
-	linearFree(buffer5);
-	linearFree(buffer6);
-	linearFree(buffer7);
-	linearFree(buffer8);
 
 	threadExit(0);
 	return;
