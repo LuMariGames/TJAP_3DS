a849e9af0746ab73edb96283c858706fed35e44d
LuMariGames <kai.hono426@gmail.com>
2025-01-23 11:22:01 +0900

    Update notes.cpp

--- source/notes.cpp
+++ source/notes.cpp
@@ -20,7 +20,7 @@ static int find_notes_id(), find_line_id(), make_roll_start(int NotesId), make_r
 int make_balloon_end(int NotesId);
 void notes_calc(bool isDon, bool isKatsu, double bpm, double CurrentTimeNotes, int cnt, C2D_Sprite sprites[SPRITES_NUMER]);
 static void notes_draw(C2D_Sprite sprites[SPRITES_NUMER]), make_balloon_break(), delete_notes(int i);
-void init_notes(TJA_HEADER_T TJA_Header), draw_judge(double CurrentTimeNotes, C2D_Sprite sprites[SPRITES_NUMER]), void notes_sort(), delete_roll(int i);
+void init_notes(TJA_HEADER_T TJA_Header), draw_judge(double CurrentTimeNotes, C2D_Sprite sprites[SPRITES_NUMER]), notes_sort(), delete_roll(int i);
 
 NOTES_T Notes[NOTES_MAX];
 COMMAND_T Command;
