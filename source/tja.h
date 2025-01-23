b524c5bef32309f31391729bc933a79f3446e043
LuMariGames <kai.hono426@gmail.com>
2025-01-23 12:07:54 +0900

    Update tja.h

--- source/tja.h
+++ source/tja.h
@@ -34,8 +34,8 @@ typedef struct {
 
 void load_tja_head(int course, LIST_T Song), load_tja_notes(int course, LIST_T Song), init_tja();
 void tja_to_notes(bool isDnon, bool isKatsu, int count, C2D_Sprite sprites[SPRITES_NUMER]);
-void get_command_value(char* buf, COMMAND_T *Command), get_tja_header(TJA_HEADER_T *TJA_Header);
-void load_tja_head_simple(LIST_T *List);
+void load_tja_head_simple(LIST_T *List), get_tja_header(TJA_HEADER_T *TJA_Header);
+static void get_command_value(char* buf, COMMAND_T *Command);
 double get_FirstMeasureTime();
 int get_MeasureId_From_OriginalId(int id);
 bool get_isBranch();
