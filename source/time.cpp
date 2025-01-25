e55b4ed272b4be8ad829f65ee3cec19520c37954
LuMariGames <kai.hono426@gmail.com>
2025-01-25 11:08:21 +0900

    Update time.cpp

--- source/time.cpp
+++ source/time.cpp
@@ -22,7 +22,7 @@ double PreTime[TIME_NUM],Time[TIME_NUM],CurrentTime[TIME_NUM],IniVorbisTime[TIME
 double get_current_time(int id) {
 	
 	if ((id == 0 || id == 1) && get_isMusicStart() == true) { //メインのカウントの時はVorbis基準の時間を返す 要曲終了時の処理
-		if (isStop[id] != 1) stop_time(id);
+		//if (isStop[id] != 1) stop_time(id);
 		if (CurrentTime[id] == 0 && Time[id] == 0 && IniVorbisTime[id] == 0) IniVorbisTime[id] = getVorbisTime();
 		CurrentTime[id] = Time[id] + (getVorbisTime() - IniVorbisTime[id]);
 		return CurrentTime[id] * mspeed(); 
