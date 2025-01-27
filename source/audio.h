7e58713ad248f01a00e5a8ef06bbda7a43386266
LuMariGames <kai.hono426@gmail.com>
2025-01-27 16:57:07 +0900

    Update audio.h

--- source/audio.h
+++ source/audio.h
@@ -2,22 +2,6 @@
 void load_sound();
 void sd_load_sound();
 //void load_combo();
-int play_sound(int id) {
-
-	if (sound[id].audiochannel == -1) {
-		printf("No available audio channel\n");
-		return -1;
-	}
-	ndspChnWaveBufClear(sound[id].audiochannel);
-	/*ndspChnReset(sound[id].audiochannel);
-	ndspChnInitParams(sound[id].audiochannel);
-	ndspChnSetMix(sound[id].audiochannel, sound[id].mix);
-	ndspChnSetInterp(sound[id].audiochannel, sound[id].interp);
-	ndspChnSetRate(sound[id].audiochannel, sound[id].rate);
-	ndspChnSetFormat(sound[id].audiochannel, NDSP_CHANNELS(sound[id].channels) | NDSP_ENCODING(sound[id].encoding));*/
-	ndspChnWaveBufAdd(sound[id].audiochannel, &waveBuf[id]);
-
-	return 0;
-}
+int play_sound(int id);
 void exit_music();
 int ogg_check();
