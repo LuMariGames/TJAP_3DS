b489c5070af539a0f33ff952cb7d83439fb4e1c9
LuMariGames <kai.hono426@gmail.com>
2025-01-27 16:45:35 +0900

    Delete source directory

--- source/time.h
+++
@@ -1,9 +0,0 @@
-﻿#pragma once
-double get_current_time(int id);
-void time_ini();
-void draw_fps();
-void stop_time(int id);
-void restart_time(int id);
-int get_time_isStop(int id);
-void toggle_time(int id);
-//double calc_vorbis_time(double CurrentTimeNotes);
