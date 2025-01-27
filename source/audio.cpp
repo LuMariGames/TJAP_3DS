e715a351a49782be8c01a69b140f5bfb89b35aa7
LuMariGames <kai.hono426@gmail.com>
2025-01-27 16:57:31 +0900

    Update audio.cpp

--- source/audio.cpp
+++ source/audio.cpp
@@ -330,6 +330,24 @@ void sd_load_sound() {
 	}
 }*/
 
+int play_sound(int id) {
+
+	if (sound[id].audiochannel == -1) {
+		printf("No available audio channel\n");
+		return -1;
+	}
+	ndspChnWaveBufClear(sound[id].audiochannel);
+	/*ndspChnReset(sound[id].audiochannel);
+	ndspChnInitParams(sound[id].audiochannel);
+	ndspChnSetMix(sound[id].audiochannel, sound[id].mix);
+	ndspChnSetInterp(sound[id].audiochannel, sound[id].interp);
+	ndspChnSetRate(sound[id].audiochannel, sound[id].rate);
+	ndspChnSetFormat(sound[id].audiochannel, NDSP_CHANNELS(sound[id].channels) | NDSP_ENCODING(sound[id].encoding));*/
+	ndspChnWaveBufAdd(sound[id].audiochannel, &waveBuf[id]);
+
+	return 0;
+}
+
 void exit_music() {
 
 	ndspChnWaveBufClear(sound[0].audiochannel);
