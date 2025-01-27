b663c32081ae0e893042aabea764cf285787738e
LuMariGames <kai.hono426@gmail.com>
2025-01-27 15:52:26 +0900

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -49,12 +49,11 @@ void init_main() {
 	C2D_Init(C2D_DEFAULT_MAX_OBJECTS);
 	C2D_Prepare();
 	g_dynamicBuf = C2D_TextBufNew(4096);
-	gfxSetWide(true);
+	gfxSetWide(false);
 	osSetSpeedupEnable(true);
 	gfxSetDoubleBuffering(GFX_TOP, true);
 	gfxScreenSwapBuffers(GFX_TOP, true);
 	C3D_FrameRate(60);
-	APT_SetAppCpuTimeLimit(100);
 }
 
 void exit_main() {
