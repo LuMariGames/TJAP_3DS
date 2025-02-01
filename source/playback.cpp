f76e1688d9cf4628974d90c7dce9f39b4053cfb7
LuMariGames <kai.hono426@gmail.com>
2025-02-01 11:15:00 +0900

    Update playback.cpp

--- source/playback.cpp
+++ source/playback.cpp
@@ -143,7 +143,7 @@ void playFile(void* infoIn){
 	ndspChnReset(CHANNEL);
 	ndspChnWaveBufClear(CHANNEL);
 	ndspSetOutputMode(NDSP_OUTPUT_STEREO);
-	ndspChnSetInterp(CHANNEL, NDSP_INTERP_POLYPHASE);
+	ndspChnSetInterp(CHANNEL, NDSP_INTERP_NONE);
 	ndspChnSetRate(CHANNEL, (*decoder.rate)() * mspeed());
 	ndspChnSetFormat(CHANNEL,
 			(*decoder.channels)() == 2 ? NDSP_FORMAT_STEREO_PCM16 :
