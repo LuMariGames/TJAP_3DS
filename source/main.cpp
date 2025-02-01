07ca6ac504e4a26a11826a2daa1e977d22a97237
LuMariGames <kai.hono426@gmail.com>
2025-02-01 11:47:05 +0900

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -50,7 +50,7 @@ void init_main() {
 	C2D_Prepare();
 	g_dynamicBuf = C2D_TextBufNew(4096);
 	gfxSetWide(false);
-	osSetSpeedupEnable(true);
+	osSetSpeedupEnable(false);
 	gfxSetDoubleBuffering(GFX_TOP, true);
 }
 
