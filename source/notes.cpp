99c72c87e142190bab53583a05bd020c72e14963
LuMariGames <kai.hono426@gmail.com>
2025-02-02 16:35:39 +0900

    Update notes.cpp

--- source/notes.cpp
+++ source/notes.cpp
@@ -343,7 +343,7 @@ void notes_main(bool isDon, bool isKatsu, char tja_notes[MEASURE_MAX][NOTES_MEAS
 	float jut1 = 0.0f, jut2 = 0.0f;
 	for (int i = 0; i < BARLINE_MAX; ++i) {
 		jut1 = Measure[BarLine[i].measure].judge_time;
-		if (jut2 > CurrentTimeNotes && jut2 > jut1) continue;
+		if (jut1 > CurrentTimeNotes && jut2 > jut1) continue;
 		NowBPM = Measure[BarLine[i].measure].bpm;
 		jut2 = jut1;
 	}
