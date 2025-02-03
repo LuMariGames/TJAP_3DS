9b1445194b27de1bce9f4bef16cbf246eac5d39b
LuMariGames <kai.hono426@gmail.com>
2025-02-03 13:38:36 +0900

    Update tja.cpp

--- source/tja.cpp
+++ source/tja.cpp
@@ -628,8 +628,8 @@ void load_tja_notes(int course, LIST_T Song) {
 				Measure[MeasureCount].measure = NextMeasure;
 				Measure[MeasureCount].scroll = scroll;
 				Measure[MeasureCount].judge_time = 240.0 / bpm * measure * percent + PreJudge + delay;
-				Measure[MeasureCount].pop_time = Measure[MeasureCount].judge_time - (240.0 / Measure[MeasureCount].bpm)*(NOTES_JUDGE_RANGE / NOTES_AREA);
-				Measure[MeasureCount].create_time = Measure[MeasureCount].judge_time - (240.0 / Measure[MeasureCount].bpm)*(NOTES_JUDGE_RANGE / (NOTES_AREA * fabs(scroll)));
+				Measure[MeasureCount].pop_time = Measure[MeasureCount].judge_time - (240.0 * NOTES_JUDGE_RANGE) / (Measure[MeasureCount].bpm * NOTES_AREA);
+				Measure[MeasureCount].create_time = Measure[MeasureCount].judge_time - (240.0 * NOTES_JUDGE_RANGE) / (Measure[MeasureCount].bpm * (NOTES_AREA * fabs(scroll)));
 				Measure[MeasureCount].isDispBarLine = isDispBarLine;
 				Measure[MeasureCount].branch = BranchCourse;
 
@@ -673,7 +673,7 @@ void load_tja_notes(int course, LIST_T Song) {
 						Measure[MeasureCount].pop_time = BeforeBranchPopTime;
 						PreJudge = BeforeBranchPreJudge;
 						isDispBarLine = BeforeBranchIsDispBarLine;
-						FirstMultiMeasure =BeforeBranchFirstMultiMeasure;
+						FirstMultiMeasure = BeforeBranchFirstMultiMeasure;
 						isNoComma = BeforeBranchIsNoComma;
 						NotesCount = BeforeBranchNotesCount;
 						percent = BeforeBranchPercent;
@@ -703,9 +703,8 @@ void load_tja_notes(int course, LIST_T Song) {
 								(240.0 / Measure[Measure[MeasureCount].firstmeasure + i - 1].bpm * Measure[Measure[MeasureCount].firstmeasure + i - 1].measure)
 								* Measure[Measure[MeasureCount].firstmeasure + i - 1].notes_count / Measure[Measure[MeasureCount].firstmeasure].max_notes;	//delayはとりあえず放置
 
-							//Measure[Measure[MeasureCount].firstmeasure + i].count_time  = Measure[Measure[MeasureCount].firstmeasure + i].judge_time + (240.0 / Measure[Measure[MeasureCount].firstmeasure + i].bpm);
-							Measure[Measure[MeasureCount].firstmeasure + i].pop_time    = Measure[Measure[MeasureCount].firstmeasure + i].judge_time - (240.0 / Measure[Measure[MeasureCount].firstmeasure + i].bpm)*(NOTES_JUDGE_RANGE / (NOTES_AREA));
-							Measure[Measure[MeasureCount].firstmeasure + i].create_time = Measure[Measure[MeasureCount].firstmeasure + i].judge_time - (240.0 / Measure[Measure[MeasureCount].firstmeasure + i].bpm)*(1.0*NOTES_JUDGE_RANGE / (NOTES_AREA * fabs(Measure[Measure[MeasureCount].firstmeasure + i].scroll)));
+							Measure[Measure[MeasureCount].firstmeasure + i].pop_time    = Measure[Measure[MeasureCount].firstmeasure + i].judge_time - (240.0 * NOTES_JUDGE_RANGE) / (Measure[Measure[MeasureCount].firstmeasure + i].bpm * NOTES_AREA);
+							Measure[Measure[MeasureCount].firstmeasure + i].create_time = Measure[Measure[MeasureCount].firstmeasure + i].judge_time - (240.0 * NOTES_JUDGE_RANGE) / (Measure[Measure[MeasureCount].firstmeasure + i].bpm * (NOTES_AREA * fabs(Measure[Measure[MeasureCount].firstmeasure + i].scroll)));
 							percent = (double)Measure[Measure[MeasureCount].firstmeasure + i].notes_count / (double)Measure[Measure[MeasureCount].firstmeasure].max_notes;
 
 							Measure[Measure[MeasureCount].firstmeasure + i].isDispBarLine = false;	//最初の小節は小節線をオフにしない
