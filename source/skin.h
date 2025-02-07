284a542b74ade01057cdf1cd92e46b856a485411
LuMariGames <kai.hono426@gmail.com>
2025-02-07 11:46:48 +0900

    Update skin.h

--- source/skin.h
+++ source/skin.h
@@ -1,7 +1,7 @@
 typedef struct {
 
 	int don_x,don_y,don_gogo_x,don_gogo_y,
-	d1num,d2num,d3num,d1total,d2total,d3total,d1anime[120],d2anime[60],d3anime[60];	//踊り子関連
+	d1num,d2num,d3num,d1total,d2total,d3total,d1anime[128],d2anime[128],d3anime[128];	//踊り子関連
 } SKIN_T;
 
 void get_skin(SKIN_T *TMP);
