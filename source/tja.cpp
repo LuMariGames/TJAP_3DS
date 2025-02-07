bc562b1a6943ea0be8ad0d0d214e79f0271ee4ab
LuMariGames <kai.hono426@gmail.com>
2025-02-08 08:11:31 +0900

    Update tja.cpp

--- source/tja.cpp
+++ source/tja.cpp
@@ -732,7 +732,7 @@ void load_tja_notes(int course, LIST_T Song) {
 			case COMMAND_SECTION:
 			case COMMAND_GOGOSTART:
 			case COMMAND_GOGOEND:
-				int n = Measure[i].notes + 1;
+				int n = i + 1;
 				while (n <= MeasureMaxNumber && tja_notes[n][0] == '#') ++n;
 				Measure[i].judge_time = Measure[n].judge_time;
 				break;
