fbb2f772164ff173ea60f9720e1cf7afee566d27
LuMariGames <kai.hono426@gmail.com>
2025-02-22 09:27:10 +0900

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -49,7 +49,7 @@ void init_main() {
 
 	romfsInit();
 	gfxInitDefault();
-	C3D_Init(C3D_DEFAULT_CMDBUF_SIZE);
+	C3D_Init(C3D_CMDBUF_SIZE);
 	C2D_Init(C2D_DEFAULT_MAX_OBJECTS);
 	C2D_Prepare();
 	g_dynamicBuf = C2D_TextBufNew(4096);
