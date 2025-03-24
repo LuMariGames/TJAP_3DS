8429be42d14348cb1fea98acb2d45af3f7391f2c
LuMariGames <kai.hono426@gmail.com>
2025-03-24 16:03:26 +0900

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -275,7 +275,7 @@ int main() {
 			if (!SelectedSong.course_exist[course]) load_tja_notes(-1, SelectedSong);
 			else load_tja_notes(course, SelectedSong);
 			time_ini();
-			offset = TJA_Header.offset + Option.offset - 0.128;
+			offset = TJA_Header.offset + Option.offset - 0.064;
 			notes_cnt = 0;
 			isNotesStart = false, isMusicStart = false, isPlayMain = false;
 			FirstMeasureTime = INT_MAX;
