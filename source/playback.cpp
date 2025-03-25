6261b96631133f0b5d7679484f28c379f62e238d
LuMariGames <kai.hono426@gmail.com>
2025-03-25 12:07:14 +0900

    Update playback.cpp

--- source/playback.cpp
+++ source/playback.cpp
@@ -165,8 +165,6 @@ void playFile(void* infoIn){
 	waveBuf[3].data_vaddr = &buffer4[0];
 	svcFlushProcessDataCache(CUR_PROCESS_HANDLE, (u32)buffer4, decoder.vorbis_buffer_size * sizeof(int16_t));
 
-	svcSleepThread((osGetTime() - SetTime) * 1000);
-
 	ndspChnWaveBufAdd(CHANNEL, &waveBuf[0]);
 	ndspChnWaveBufAdd(CHANNEL, &waveBuf[1]);
 	ndspChnWaveBufAdd(CHANNEL, &waveBuf[2]);
