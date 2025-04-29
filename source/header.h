bf85227d2cb073bdd662ad2f7ec83f25a484397a
LuMariGames <kai.hono426@gmail.com>
2025-04-29 10:02:05 +0900

    Update header.h

--- source/header.h
+++ source/header.h
@@ -13,7 +13,7 @@
 
 #define C3D_CMDBUF_SIZE 0x40000
 #define VERSION "1.5.2"
-#define DECODE_COREID 0
+#define DECODE_COREID 1
 #define DECODE_MEM 65536
 #define DEFAULT_DIR "sdmc:/tjafiles/"
 #define NOTDEF_DIR "romfs:/tjafiles/"
@@ -47,7 +47,7 @@
 #define LIST_MAX 16384		//選曲リストの最大数
 #define GENRE_MAX 512		//ジャンルの最大数
 
-#define DEFAULT_BUFFER_SIZE 8192
+#define DEFAULT_BUFFER_SIZE 16384
 
 enum NOTES_KND {
 
