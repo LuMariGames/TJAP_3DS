60e4abec258c4148bea815663ddfb7b518c7fc8e
LuMariGames <kai.hono426@gmail.com>
2025-06-14 16:02:17 +0900

    Update select.h

--- source/select.h
+++ source/select.h
@@ -18,6 +18,6 @@ typedef struct {
 void load_file_main(void *arg), get_SelectedId(LIST_T *TMP, int *arg), get_result(RESULT_T *Result), select_ini(), update_cursor(int knd);
 void draw_select_text(float x, float y, const char* text, int color = 0xffffff), disp_file_list();
 void draw_option_text(float x, float y, const char *text, bool state, float *width, float *height,float sizex=0.7,float sizey = 0.7);
-void draw_result_text(float x, float y, float size, const char *text);
+void draw_result_text(float x, float y, float size, const char *text), randselect();
 void calc_result_text(const char *text, float *width, float *height);
 bool get_isGameStart();
