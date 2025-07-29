412fc7a4d36f18d89df321e24f38ad1a032e942c
LuMariGames <kai.hono426@gmail.com>
2025-07-29 14:33:27 +0900

    Update notes.cpp

--- source/notes.cpp
+++ source/notes.cpp
@@ -1278,6 +1278,6 @@ void newfont() {
 void fontfree() {
 	C2D_TextBufDelete(g_NotesText);
 	C2D_FontFree(font);
-	Notes.clear();
-	std::vector<NOTES_T>().swap(Notes);
+	Notes.resize(0);
+	Notes.shrink_to_fit();
 }
