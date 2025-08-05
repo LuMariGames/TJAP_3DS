5f80e7fc3d46d32f27cdc2592523e9ee206d5a05
LuMariGames <kai.hono426@gmail.com>
2025-08-05 14:19:46 +0900

    Update main.h

--- source/main.h
+++ source/main.h
@@ -5,5 +5,6 @@ bool get_isPause(),get_isMusicStart();
 char* get_buffer();
 int powi(int x, int y),pause_window(touchPosition tp, unsigned int key),
 message_window(touchPosition tp, unsigned int key,int text);
+double starttime();
 
 #define BUFFER_SIZE 160 //バッファ用文字列のサイズ
