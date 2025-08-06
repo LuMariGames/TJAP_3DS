67ac928b1af71c97e2581d775915d9cffe15d109
LuMariGames <kai.hono426@gmail.com>
2025-08-06 16:30:30 +0900

    Update select.cpp

--- source/select.cpp
+++ source/select.cpp
@@ -171,10 +171,7 @@ void disp_file_list() {
 
 	for (int i = 0; i < SongNumber; ++i) {
 
-		if (strcmp(Option.SongTitle, "\0") == 0) {
-			continue;
-		}
-		else if (strstr(List[i].title, Option.SongTitle) == NULL) {
+		if (strcmp(Option.SongTitle, NULL) != 0 && strstr(List[i].title, Option.SongTitle) == NULL) {
 			continue;
 		}
 		isGenre = false;
