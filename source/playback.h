83d94bc4cdbd73976b5759151bb4e92b06175121
LuMariGames <kai.hono426@gmail.com>
2025-08-13 14:23:55 +0900

    Update playback.h

--- source/playback.h
+++ source/playback.h
@@ -17,7 +17,7 @@ struct decoder_fn{
 	int (* init)(const char* file);
 	uint32_t (* rate)(void);
 	uint8_t (* channels)(void);
-	size_t vorbis_buffer_size;
+	size_t buffSize;
 	uint64_t (* decode)(void*);
 	void (* exit)(void);
 	long (* bitrate)(void);
