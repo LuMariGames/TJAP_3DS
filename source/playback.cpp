53094a91eb4c36c1ab4987969007386006e3afde
LuMariGames <kai.hono426@gmail.com>
2025-08-14 18:52:03 +0900

    Update playback.cpp

--- source/playback.cpp
+++ source/playback.cpp
@@ -220,7 +220,6 @@ inline int changeFile(const char* ep_file, struct playbackInfo_t* playbackInfo, 
 		threadJoin(thread, U64_MAX);
 		threadFree(thread);
 		thread = NULL;
-		APT_SetAppCpuTimeLimit(5);
 	}
 
 	if (ep_file == NULL || playbackInfo == NULL)
