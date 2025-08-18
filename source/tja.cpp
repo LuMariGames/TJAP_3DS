67863152d4dd17ec03b34fc129078ff428453b86
LuMariGames <kai.hono426@gmail.com>
2025-08-18 18:55:48 +0900

    Update tja.cpp

--- source/tja.cpp
+++ source/tja.cpp
@@ -696,7 +696,7 @@ void load_tja_notes(int course, LIST_T Song) {
 				Measure[MeasureCount].create_time = Measure[MeasureCount].judge_time + (isSudden ? (240.0 / NextBpm - sudntime) : 0) - (240.0 * NOTES_JUDGE_RANGE) / (Measure[MeasureCount].bpm * (NOTES_AREA * fabs(scroll * Option.speed)));
 				Measure[MeasureCount].isDispBarLine = isDispBarLine;
 				Measure[MeasureCount].branch = BranchCourse;
-				strlcpy(Measure[MeasureCount].lyric, ly, strlen(ly) - 1);
+				strlcpy(Measure[MeasureCount].lyric, ly, strlen(ly));
 
 				if (tja_notes[tja_cnt][0] == '#') {
 
