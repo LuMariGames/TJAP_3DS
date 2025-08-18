dcedb9aa24539ac223bc71083176664afe5a5d67
LuMariGames <kai.hono426@gmail.com>
2025-08-18 19:27:48 +0900

    Update tja.cpp

--- source/tja.cpp
+++ source/tja.cpp
@@ -535,7 +535,7 @@ void load_tja_notes(int course, LIST_T Song) {
 	double bpm = Current_Header.bpm,NextBpm = bpm,measure = 1,scroll = 1,NextMeasure = 1,delay = 0,percent = 1,sudntime = 0,movetime = 0,
 		BeforeBranchJudgeTime = 0,BeforeBranchCreateTime = 0,BeforeBranchPopTime = 0,BeforeBranchPreJudge = 0,BeforeBranchBpm = 0,BeforeBranchMoveTime = 0,
 		BeforeBranchDelay = 0,BeforeBranchMeasure = 0,BeforeBranchScroll = 1,BeforeBranchNextBpm = 0,BeforeBranchNextMeasure = 0,BeforeBranchPercent = 1;
-	char ly[64] = "", Beforely[64] = "";
+	char ly[64] = " ", Beforely[64] = " ";
 
 	if (course == -1) isCourseMatch = true;		//コース表記なし
 
@@ -665,7 +665,7 @@ void load_tja_notes(int course, LIST_T Song) {
 						isEnd = true;
 						break;
 					case COMMAND_LYRIC:
-						strlcpy(ly, Command.value_s + 1, sizeof(ly));
+						strlcpy(ly, Command.value_s, sizeof(ly));
 						break;
 					}
 				}
@@ -695,7 +695,7 @@ void load_tja_notes(int course, LIST_T Song) {
 				Measure[MeasureCount].create_time = Measure[MeasureCount].judge_time + (isSudden ? (240.0 / NextBpm - sudntime) : 0) - (240.0 * NOTES_JUDGE_RANGE) / (Measure[MeasureCount].bpm * (NOTES_AREA * fabs(scroll * Option.speed)));
 				Measure[MeasureCount].isDispBarLine = isDispBarLine;
 				Measure[MeasureCount].branch = BranchCourse;
-				strlcpy(Measure[MeasureCount].lyric, ly, strlen(ly));
+				strlcpy(Measure[MeasureCount].lyric, ly, strlen(ly) - 1);
 
 				if (tja_notes[tja_cnt][0] == '#') {
 
