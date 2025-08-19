b9051e8fea016c7f0c8ce0df2f19e3e3074d16e7
LuMariGames <kai.hono426@gmail.com>
2025-08-19 15:56:04 +0900

    Update tja.cpp

--- source/tja.cpp
+++ source/tja.cpp
@@ -955,7 +955,13 @@ void get_command_value(char* buf, COMMAND_T *Command) {
 		else if (strcmp(command, "LEVELHOLD") == 0) Command->knd = COMMAND_LEVELHOLD;
 		else if (strcmp(command, "BARLINEOFF") == 0) Command->knd = COMMAND_BARLINEOFF;
 		else if (strcmp(command, "BARLINEON") == 0) Command->knd = COMMAND_BARLINEON;
-		else if (strcmp(command, "LYRIC") == 0) Command->knd = COMMAND_LYRIC;
+		else if (strcmp(command, "LYRIC") == 0) {
+			Command->knd = COMMAND_LYRIC;
+			if (buf[7] != '\n' && buf[7] != '\r') {
+				strlcpy(value, buf + 7, strlen(buf) - 8);
+				Command->value_s = value;
+			}
+		}
 		/*else if (strcmp(command, "BMSCROLL") == 0) Command->knd = COMMAND_BMSCROLL;
 		else if (strcmp(command, "HBSCROLL") == 0) Command->knd = COMMAND_HBSCROLL;*/
 		else Command->knd = -1;
