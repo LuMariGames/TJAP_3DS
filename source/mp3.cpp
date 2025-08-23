f8c82a6514394ac902269b0c4c3ed1f0242ac403
LuMariGames <kai.hono426@gmail.com>
2025-08-23 13:08:48 +0900

    Update mp3.cpp

--- source/mp3.cpp
+++ source/mp3.cpp
@@ -50,9 +50,9 @@ int initMp3(const char* file)
 	}
 	mpg123_format_none(mh);
 	mpg123_format(mh, rate, channels, encoding);
-	mpg123_seek(mh, (int)(rate * SeekTime), SEEK_SET);
 	*buffSize = mpg123_outblock(mh) * 8;
 	mpg123_param(mh, MPG123_DOWN_SAMPLE, 1, 1.0);
+	mpg123_seek(mh, (off_t)(rate * SeekTime), SEEK_SET);
 	return 0;
 }
 
