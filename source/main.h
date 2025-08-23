ebdfd54e941dfad773d12e24926e8a0132576a5d
LuMariGames <kai.hono426@gmail.com>
2025-08-23 15:41:11 +0900

    Update main.h

--- source/main.h
+++ source/main.h
@@ -1,7 +1,7 @@
 ﻿#pragma once
 
 void draw_debug(float x, float y, const char *text),debug_touch(int x, int y);
-bool get_isPause(),get_isMusicStart();;
+bool get_isPause(),get_isMusicStart();
 char* get_buffer();
 int powi(int x, int y),pause_window(touchPosition tp, unsigned int key),
 message_window(touchPosition tp, unsigned int key,int text);
