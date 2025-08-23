e93436353ec8655721a4e0793dcdc0ff84145a8c
LuMariGames <kai.hono426@gmail.com>
2025-08-23 10:22:05 +0900

    Update main.h

--- source/main.h
+++ source/main.h
@@ -1,12 +1,10 @@
 ﻿#pragma once
 
-void draw_debug(float x, float y, const char *text);
-bool get_isPause();
-void debug_touch(int x, int y);
-bool get_isMusicStart();
+void draw_debug(float x, float y, const char *text),debug_touch(int x, int y);
+bool get_isPause(),get_isMusicStart();;
 char* get_buffer();
-int powi(int x, int y);
-int pause_window(touchPosition tp, unsigned int key);
-int message_window(touchPosition tp, unsigned int key,int text);
+int powi(int x, int y),pause_window(touchPosition tp, unsigned int key),
+message_window(touchPosition tp, unsigned int key,int text);
+double starttime();
 
 #define BUFFER_SIZE 160 //バッファ用文字列のサイズ
