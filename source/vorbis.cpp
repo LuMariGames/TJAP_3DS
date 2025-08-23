594f79d5baf558910860aae3204389f89ca51d91
LuMariGames <kai.hono426@gmail.com>
2025-08-23 15:34:25 +0900

    Update vorbis.cpp

--- source/vorbis.cpp
+++ source/vorbis.cpp
@@ -108,8 +108,6 @@ double getVorbisTime() {
 	else return -1000;
 }
 int setVorbisTime(double after_time) {
-
-	if (ov_time_total(&vorbisFile, -1) >= after_time) return ov_pcm_seek(&vorbisFile, 0);
 	return ov_pcm_seek(&vorbisFile, (after_time * vi->rate));
 }
 
