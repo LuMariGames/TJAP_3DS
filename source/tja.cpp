d485a0c84a54f327a6a2439f51621a72cb9f2592
LuMariGames <kai.hono426@gmail.com>
2025-08-26 08:26:13 +0900

    Update tja.cpp

--- source/tja.cpp
+++ source/tja.cpp
@@ -46,7 +46,13 @@ void init_tja() {
 
 	tja_cnt = 0;
 	MeasureMaxNumber = 0;
-	MainFirstMeasureTime = 0;
+	if (Option.measure > 0) {
+		MainFirstMeasureTime = calc_first_measure_time();
+		sort_measure_insertion(Measure, MEASURE_MAX);
+		stme = get_MeasureId_From_OriginalId(stme);
+		MainFirstMeasureTime = Measure[stme].bpm;
+	}
+	else MainFirstMeasureTime = 0;
 	init_measure_structure();
 	isBranch = false;
 }
@@ -539,7 +545,7 @@ void load_tja_notes(int course, LIST_T Song) {
 	OPTION_T Option;
 	get_option(&Option);
 
-	double bpm = Current_Header.bpm,NextBpm = bpm,measure = 1,scroll = 1,NextMeasure = 1,delay = 0,percent = 1,sudntime = 0,movetime = 0,
+	double bpm = ((Option.measure > 0) ? MainFirstMeasureTime : Current_Header.bpm),NextBpm = bpm,measure = 1,scroll = 1,NextMeasure = 1,delay = 0,percent = 1,sudntime = 0,movetime = 0,
 		BeforeBranchJudgeTime = 0,BeforeBranchCreateTime = 0,BeforeBranchPopTime = 0,BeforeBranchPreJudge = 0,BeforeBranchBpm = 0,BeforeBranchMoveTime = 0,
 		BeforeBranchDelay = 0,BeforeBranchMeasure = 0,BeforeBranchScroll = 1,BeforeBranchNextBpm = 0,BeforeBranchNextMeasure = 0,BeforeBranchPercent = 1;
 	std::string ly = "", Beforely = "";
