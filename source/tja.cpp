35f5bacf6e97683883a65c4bf359e28b8048a39d
LuMariGames <kai.hono426@gmail.com>
2025-08-26 09:31:07 +0900

    Update tja.cpp

--- source/tja.cpp
+++ source/tja.cpp
@@ -1003,5 +1003,5 @@ double get_StartTime() {
 
 	OPTION_T Option;
 	get_option(&Option);
-	return ((Option.measure > 0) ? ((Current_Header.offset + Option.offset) * -1.0) + (Measure[stme].judge_time - (240.0 / Measure[stte].bpm)) : 0);
+	return ((Option.measure > 0) ? ((Current_Header.offset + Option.offset) * -1.0) + (Measure[stme].judge_time - (240.0 / Current_Header.bpm)) : 0);
 }
