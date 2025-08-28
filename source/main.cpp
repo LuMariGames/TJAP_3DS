e28fd96c0f606d82e46923a9b2b49ca9c68943c4
LuMariGames <kai.hono426@gmail.com>
2025-08-28 15:18:39 +0900

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -159,8 +159,8 @@ int main() {
 			if (tp.px != 0 && tp.py != 0) {	//タッチ位置の取得
 
 				PreTouch_x = touch_x, PreTouch_y = touch_y;
+				touch_x = tp.px, touch_y = tp.py;
 				if (key & KEY_TOUCH) {
-					touch_x = tp.px, touch_y = tp.py;
 					int dx = (touch_x - PreTouch_x), dy = (touch_y - PreTouch_y);
 					if (touch_x != dx) touch_x += dx;
 					if (touch_y != dy) touch_y += dy;
@@ -759,6 +759,7 @@ inline int dancer_time_count(double TIME, int NUM) noexcept {
 double starttime() {
 	return get_StartTime();
 }
+
 
 
 
