14c3b8a3e4b18db1cab7b61699660855f65bf3d3
LuMariGames <kai.hono426@gmail.com>
2025-08-29 14:17:31 +0900

    Update main.h

--- source/main.h
+++ source/main.h
@@ -4,7 +4,7 @@ void draw_debug(float x, float y, const char *text),debug_touch(int x, int y);
 bool get_isPause(),get_isMusicStart();
 char* get_buffer();
 int powi(int x, int y),pause_window(touchPosition tp, unsigned int key),
-message_window(touchPosition tp, unsigned int key,int text);
+message_window(touchPosition tp, unsigned int key,int text),exist_file(const char* path);
 double starttime();
 
 #define BUFFER_SIZE 160 //バッファ用文字列のサイズ
