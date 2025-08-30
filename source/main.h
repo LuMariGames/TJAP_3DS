6f593a32729d92352fcbcd5adcbb2709f3128040
LuMariGames <kai.hono426@gmail.com>
2025-08-30 10:20:15 +0900

    Update main.h

--- source/main.h
+++ source/main.h
@@ -3,7 +3,7 @@
 void draw_debug(float x, float y, const char *text),debug_touch(int x, int y);
 bool get_isPause(),get_isMusicStart();
 char* get_buffer();
-int powi(int x, int y),pause_window(touchPosition tp, unsigned int key),
+int powi(int x, int y),pause_window(touchPosition tp, unsigned int key),get_scene(),
 message_window(touchPosition tp, unsigned int key,int text),exist_file(const char* path);
 double starttime();
 
