c043c003d5c063ce9cadaa9b48aeb8aa040594d7
LuMariGames <kai.hono426@gmail.com>
2025-08-30 12:00:20 +0900

    Update select.cpp

--- source/select.cpp
+++ source/select.cpp
@@ -109,7 +109,7 @@ inline void load_file_list(const char* path) {
 					if (strstr(dp->d_name, ".tja") != NULL) {
 
 						strlcpy(List[SongCount].tja, dp->d_name, strlen(dp->d_name) + 1);
-						getcwd(List[SongCount].path, 256);
+						strlcpy(List[SongCount].path, path, strlen(path) + 1);
 						List[SongCount].genre = GENRE_MAX + 1;
 						load_tja_head_simple(&List[SongCount]);
 						loadend = 1;
