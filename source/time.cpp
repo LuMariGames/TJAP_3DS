9ebf6a46f86cccf09a8209b48f22037002bd6fac
LuMariGames <kai.hono426@gmail.com>
2025-09-09 10:34:17 +0900

    Update time.cpp

--- source/time.cpp
+++ source/time.cpp
@@ -21,25 +21,11 @@ double PreTime[TIME_NUM],Time[TIME_NUM],CurrentTime[TIME_NUM],IniVorbisTime[TIME
 
 double get_current_time(int id) {
 
-	/*if ((id == 0 || id == 1) && get_isMusicStart() == true) { //メインのカウントの時はVorbis基準の時間を返す 要曲終了時の処理
-		//if (isStop[id] != 1) stop_time(id);
-		if (CurrentTime[id] == 0 && Time[id] == 0 && IniVorbisTime[id]==0) IniVorbisTime[id] = getVorbisTime();
-		CurrentTime[id] = Time[id] + getVorbisTime() - IniVorbisTime[id];
-		//return CurrentTime[id]; 
-		//snprintf(get_buffer(), BUFFER_SIZE, "vbt:%.1f", CurrentTime[id]);
-		//draw_debug(100, id*10, get_buffer()); 
-	}*/
-
 	if (isStop[id] != 1) {
 
-		//旧式だけど念の為残す
-		/*if (cnt[id] == 0) OffTime[id] = osGetTime() * 0.001;
-		++cnt[id];
-		Time[id] = osGetTime() * 0.001 - OffTime[id] + PreTime[id];*/
-
-		//計式タイマー(不具合があったら旧式に戻す)
-		if (cnt[id] == 0) clock_getres(CLOCK_MONOTONIC, &tv);
-		Time[id] += tv.tv_nsec * 0.000000001 * cnt[id] + PreTime[id];
+		clock_gettime(CLOCK_MONOTONIC, &tv);
+		if (cnt[id] == 0) OffTime[id] = tv.tv_sec + tv.tv_nsec * 0.000000001;
+		Time[id] = tv.tv_sec + tv.tv_nsec * 0.000000001 - OffTime[id] + PreTime[id];
 		++cnt[id];
 	}
 	//snprintf(get_buffer(), BUFFER_SIZE, "t:%.1f", Time[id]);
@@ -54,12 +40,7 @@ void restart_time(int id) {
 void stop_time(int id) {
 
 	isStop[id] = 1;
-	/*for (int n = 0; n < 4; ++n) {
-		msec[id][n] = 0;
-	}*/
-
  	PreTime[id] = Time[id] += 0.0178571429;
-	//sec[id] = 0;
 	cnt[id] = 0;
 }
 
@@ -94,18 +75,6 @@ void draw_fps() {
 	draw_debug(0, 0, get_buffer());
 }
 
-//double preVorbisTime = -1000,startVorbisTime = -1000;
-/*double calc_vorbis_time(double CurrentTimeNotes) {
-
-	double result,vorbisTime = getVorbisTime();
-	if (vorbisTime == -1000) result = CurrentTimeNotes;	//曲開始前はそのまま返す
-	if (preVorbisTime == -1000 && vorbisTime != -1000)	startVorbisTime = CurrentTimeNotes;//曲開始時間を保存(timenotes換算)
-	if (vorbisTime != -1000) result = vorbisTime + startVorbisTime;
-
-	preVorbisTime = vorbisTime;
-	return result;
-}*/
-
 void time_ini() {
 
 	for (int i = 0; i < TIME_NUM; ++i) {
@@ -128,6 +97,4 @@ void time_ini() {
 	fps_cnt = 0;
 	fps_sum = 0;
 	fps = 0;
-	//preVorbisTime = -1000;
-	//startVorbisTime = -1000;
 }
