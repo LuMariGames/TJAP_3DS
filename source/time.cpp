b4fe11db8489c3a201b11d4d6e14ec20dfcd5b1f
LuMariGames <kai.hono426@gmail.com>
2025-09-09 10:14:37 +0900

    Update time.cpp

--- source/time.cpp
+++ source/time.cpp
@@ -38,9 +38,8 @@ double get_current_time(int id) {
 		Time[id] = osGetTime() * 0.001 - OffTime[id] + PreTime[id];*/
 
 		//計式タイマー(不具合があったら旧式に戻す)
-		clock_gettime(CLOCK_MONOTONIC, &tv);
-		if (cnt[id] == 0) OffTime[id] = tv.tv_sec + tv.tv_nsec * 0.000000001;
-		Time[id] = (1.001/60.0 * cnt[id]) + PreTime[id];
+		if (cnt[id] == 0) clock_getres(CLOCK_PROCESS_CPUTIME_ID, &ts);
+		Time[id] += tv.tv_nsec * 0.000000001 * cnt[id] + PreTime[id];
 		++cnt[id];
 	}
 	//snprintf(get_buffer(), BUFFER_SIZE, "t:%.1f", Time[id]);
