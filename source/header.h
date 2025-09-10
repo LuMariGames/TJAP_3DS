741f74050522ab1dbff91de8a20ed235432c2f47
LuMariGames <kai.hono426@gmail.com>
2025-09-10 13:35:19 +0900

    Update header.h

--- source/header.h
+++ source/header.h
@@ -14,7 +14,7 @@
 #include <iostream>
 
 #define C3D_CMDBUF_SIZE 0x40000
-#define VERSION "1.8.0(B)"
+#define VERSION "1.8.0(C)"
 #define DECODE_MEM 524288
 #define DEFAULT_DIR "sdmc:/tjafiles"
 #define NOTDEF_DIR "romfs:/tjafiles/"
