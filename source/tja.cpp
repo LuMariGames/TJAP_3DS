a14454be96f01254bfbad93516a3845e0830d092
LuMariGames <kai.hono426@gmail.com>
2025-09-28 14:07:32 +0900

    Update tja.cpp

--- source/tja.cpp
+++ source/tja.cpp
@@ -888,7 +888,7 @@ void get_tja_header(TJA_HEADER_T *TJA_Header) {
 	*TJA_Header = Current_Header;
 }
 
-void tja_to_notes(bool isDon, bool isKatsu, int count, C2D_Sprite sprites[SPRITES_NUMER]) {
+void tja_to_notes(int isDon, int isKatsu, int count, C2D_Sprite sprites[SPRITES_NUMER]) {
 
 	notes_main(isDon, isKatsu, tja_notes, Measure, count, sprites);
 }
