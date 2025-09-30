c4d8240039c8e1a03abebbb2ebe48fdfd2c4a30b
LuMariGames <kai.hono426@gmail.com>
2025-09-30 20:28:24 +0900

    Add files via upload

---
+++ notes.h
@@ -0,0 +1,50 @@
+﻿#pragma once
+#include "tja.h"
+
+typedef struct {
+	int num, notes_max, knd, roll_id, branch_id;
+	float create_time, judge_time, pop_time, bpm, scroll, x_ini;
+	bool flag, isThrough;
+	float x;
+
+} NOTES_T;
+
+typedef struct {
+	int measure;
+	float create_time, scroll, x_ini;
+	bool flag, isDisp;
+	float x;
+
+} BARLINE_T;
+
+typedef struct {
+	int id, start_id, end_id, knd;
+	float start_x, end_x;
+	bool flag;
+
+}ROLL_T;
+
+typedef struct {
+	int id, start_id, end_id,
+		need_hit, current_hit;
+	bool flag;
+
+}BALLOON_T;
+
+typedef struct {
+	int knd, course;
+	float x, y;
+	bool next, wait;
+
+}BRANCH_T;
+
+void notes_main(
+	int isDon, int isKatsu,
+	char tja_notes[MEASURE_MAX][NOTES_MEASURE_MAX],
+	MEASURE_T Measure[MEASURE_MAX],
+	int cnt, C2D_Sprite sprites[SPRITES_NUMER]
+),
+init_notes(TJA_HEADER_T TJA_Header),draw_title(),draw_condition(),
+newfont(),fontfree(),plus_measure(),min_measure(),set_measure();
+int ctoi(char c), get_branch_course();
+bool get_notes_finish();
