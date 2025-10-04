18fc6f7440e083ab868b5464ff0a0e3a517a7bb5
LuMariGames <kai.hono426@gmail.com>
2025-10-04 14:16:02 +0900

    Update notes.cpp

--- source/notes.cpp
+++ source/notes.cpp
@@ -354,7 +354,7 @@ void notes_main(int isDon,int isKatsu,char tja_notes[MEASURE_MAX][NOTES_MEASURE_
 				break;
 			}
 			bnc = (NotesCountMax / Measure[MeasureCount].measure) / nc;
-			nc = 1;
+			nc = bid - id + 1;
 			++MeasureCount;
 			notes_sort();	//ソート
 		}
