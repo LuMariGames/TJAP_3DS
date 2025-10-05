32375562e9ee541056fa82ed9ec14963e8f62ce4
LuMariGames <kai.hono426@gmail.com>
2025-10-05 17:40:01 +0900

    Update notes.cpp

--- source/notes.cpp
+++ source/notes.cpp
@@ -10,7 +10,7 @@
 #define AUTO_ROLL_FRAME comboVoice //オート時の連打の間隔
 
 int balloon[4][256],BalloonCount[4],TotalFailedCount,
-NowMeCount,dcd,JBS = -1,bnc = 512,nc = 512,bid = 0,id = -1;
+NowMeCount,dcd,JBS = -1,bnc = 1,nc = 512,bid = 0,id = -1;
 double bpm, offset;
 float NowBPM = 120.0f;
 extern int isBranch, comboVoice, course, stme;
@@ -1346,7 +1346,7 @@ void init_notes(TJA_HEADER_T TJA_Header) {
 	init_balloon_notes();
 	Command.data[0] = 0; Command.data[1] = 0; Command.data[2] = 0;
 	Command.knd = 0; Command.val[0] = 0; Command.val[1] = 0; Command.val[2] = 0;
-	bnc = 512, nc = 1, bid = 0, id = -1;
+	bnc = 1, nc = 512, bid = 0, id = -1;
 	bpm = TJA_Header.bpm;
 	offset = TJA_Header.offset + Option.offset;
 	NowBPM = bpm;
