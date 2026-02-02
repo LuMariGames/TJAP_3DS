0ed4089e8c186d841aa4941074d1809e4a32ba4b
LuMariGames <kai.hono426@gmail.com>
2026-02-02 09:39:28 +0900

    Update tja.cpp

--- source/tja.cpp
+++ source/tja.cpp
@@ -567,7 +567,7 @@ void white_tja(LIST_T Song) {
 		fclose(fp);
 		fp = fopen(abs_path, "w");
 		SwkbdState swkbd;
-		swkbdInit(&swkbd, SWKBD_TYPE_NORMAL, 1, -1);
+		swkbdInit(&swkbd, SWKBD_TYPE_NORMAL, 1, 16384);
 		swkbdSetInitialText(&swkbd, tja_text);
 		swkbdSetFeatures(&swkbd, SWKBD_PREDICTIVE_INPUT | SWKBD_MULTILINE);
 		swkbdInputText(&swkbd, tja_text, sizeof(tja_text));
