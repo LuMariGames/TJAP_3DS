db054e10ce265f76e655d79cfde2e6e5f45c819f
LuMariGames <mariogames0120@gmail.com>
2026-02-15 16:29:39 +0900

    Add files via upload

--- source/main.cpp
+++ source/main.cpp
@@ -109,7 +109,7 @@ C2D_Image loadPNGAsC2DImage(const char* filename) {
 	unsigned w, h;
 	unsigned error = lodepng_decode32_file(&image, &w, &h, filename);
 	if (error != 0) return (C2D_Image){0,0};
-	/*u8 *gpusrc = (u8*)linearAlloc(w*h * 4);
+	u8 *gpusrc = (u8*)linearAlloc(w*h * 4);
 	u8 *img_fix = (u8*)linearAlloc(w*h * 4);
 
 	u8* src = &image[0]; u8 *dst = gpusrc;
@@ -125,20 +125,20 @@ C2D_Image loadPNGAsC2DImage(const char* filename) {
 		*dst++ = b;
 		*dst++ = g;
 		*dst++ = r;
-	}*/
+	}
 
 	C3D_Tex* tex;
 	C3D_TexInit(tex, 512, 128, GPU_TEXCOLOR::GPU_RGBA8);
 
 	// Load the texture and bind it to the first texture unit
-	//GSPGPU_FlushDataCache(gpusrc, w*h * 4);
-	//GX_TextureCopy((u32*)gpusrc, GX_BUFFER_DIM(w, h), (u32*)img_fix, GX_BUFFER_DIM(w, h), (w*h*4), GX_TRANSFER_FLIP_VERT(1));
+	GSPGPU_FlushDataCache(gpusrc, w*h * 4);
+	GX_TextureCopy((u32*)gpusrc, GX_BUFFER_DIM(w, h), (u32*)img_fix, GX_BUFFER_DIM(w, h), (w*h*4), GX_TRANSFER_FLIP_VERT(1));
 
 	// 4. 線形メモリ（Linear）からタイル形式（Tiled）へ変換してアップロード
 	// C3D_TexUploadを使うと内部でタイリング処理が行われます
-	//C3D_TexUpload(tex, &img_fix[0]);
-	C3D_TexUpload(tex, &image[0]);
-	C3D_TexBind(0, tex);
+	C3D_TexUpload(tex, &img_fix[0]);
+	//C3D_TexUpload(tex, &image[0]);
+	//C3D_TexBind(0, tex);
 
 	// 5. 表示範囲を設定（サブテクスチャ定義）
 	Tex3DS_SubTexture* subtex = (Tex3DS_SubTexture*)malloc(sizeof(Tex3DS_SubTexture));
