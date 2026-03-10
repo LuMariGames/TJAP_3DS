dbf3fcbae8f5cd4e5a9418eb3d2411a48a0aecfa
LuMariGames <mariogames0120@gmail.com>
2026-03-10 10:48:27 +0900

    Update header.h

--- source/header.h
+++ source/header.h
@@ -171,6 +171,7 @@ enum SPRITE_NOTES_KND {	//スプライト用
 	SPRITE_POTATO_2,
 	SPRITE_TIMEBOMB,		//時爆弾音符の画像
 	SPRITE_DENDEN,			//でんでん音符の画像
+	SPRITE_RAINBOW,
 	SPRITE_BACKGROUND,		//上画面の下背景固定用
 	SPRITE_DANCER_0,		//踊り子の画像
 };
