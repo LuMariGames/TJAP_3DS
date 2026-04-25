7e5418044fd63ae59760a0d35e41f9fe1d7e0f27
LuMariGames <mariogames0120@gmail.com>
2026-04-25 12:48:52 +0900

    Update notes.cpp

--- source/notes.cpp
+++ source/notes.cpp
@@ -113,7 +113,11 @@ void notes_main(int isDon,int isKatsu,char (&tja_notes)[MEASURE_MAX][NOTES_MEASU
 						break;
 					case COMMAND_JPOSSCROLL:
 						judgedata.time=Command.val[0]*1000;
-						judgedata.move=(Command.val[1]*Command.val[2])/(Command.val[0]*1000);
+						if(judgedata.time>0)judgedata.move=(Command.val[1]*Command.val[2])/judgedata.time;
+						else {
+							judgedata.move=Command.val[1]*Command.val[2];
+							judgedata.time=1;
+						}
 						judgemove=threadCreate(change_judge,(void*)(""),8192,0x3e,0,true);
 						break;
 					case COMMAND_LEVELHOLD:
