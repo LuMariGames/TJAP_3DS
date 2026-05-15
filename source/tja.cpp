52ce6a3dfb46e5d99ad09ffcef5d831c91422161
LuMariGames <mariogames0120@gmail.com>
2026-05-16 06:33:39 +0900

    Update tja.cpp

--- source/tja.cpp
+++ source/tja.cpp
@@ -839,7 +839,7 @@ void load_tja_notes(int course,LIST_T Song){
 				Measure[MeasureCount].isDummy=isDummy;
 				Measure[MeasureCount].judge_time=240.0/bpm*measure*percent+PreJudge+delay;
 				Measure[MeasureCount].pop_time=Measure[MeasureCount].judge_time-(240.0*NOTES_JUDGE_RANGE)/(Measure[MeasureCount].bpm*NOTES_AREA);
-				Measure[MeasureCount].create_time=Measure[MeasureCount].judge_time+(isSudden ?(240.0/NextBpm-sudntime): 0)-(240.0*(420.0-NOTES_JUDGE_X))/(Measure[MeasureCount].bpm*(NOTES_AREA*fabs(scroll)));
+				Measure[MeasureCount].create_time=Measure[MeasureCount].judge_time+(isSudden ?(240.0/NextBpm-sudntime): 0)-(240.0*NOTES_JUDGE_RANGE)/(Measure[MeasureCount].bpm*(NOTES_AREA*fabs(scroll)));
 				Measure[MeasureCount].isDispBarLine=isDispBarLine;
 				Measure[MeasureCount].branch=BranchCourse;
 				Measure[MeasureCount].lyric=ly;
@@ -929,7 +929,7 @@ void load_tja_notes(int course,LIST_T Song){
 								* Measure[Measure[MeasureCount].firstmeasure+i-1].notes_count/Measure[Measure[MeasureCount].firstmeasure].max_notes;	//delayはとりあえず放置
 
 							Measure[Measure[MeasureCount].firstmeasure+i].pop_time   =Measure[Measure[MeasureCount].firstmeasure+i].judge_time-(240.0 * NOTES_JUDGE_RANGE)/(Measure[Measure[MeasureCount].firstmeasure+i].bpm * NOTES_AREA);
-							Measure[Measure[MeasureCount].firstmeasure+i].create_time=Measure[Measure[MeasureCount].firstmeasure+i].judge_time+(isSudden ?(240.0/NextBpm-sudntime): 0)-(240.0 * (420.0-NOTES_JUDGE_X))/(Measure[Measure[MeasureCount].firstmeasure+i].bpm *(NOTES_AREA * fabs(Measure[Measure[MeasureCount].firstmeasure+i].scroll)));
+							Measure[Measure[MeasureCount].firstmeasure+i].create_time=Measure[Measure[MeasureCount].firstmeasure+i].judge_time+(isSudden ?(240.0/NextBpm-sudntime): 0)-(240.0*NOTES_JUDGE_RANGE)/(Measure[Measure[MeasureCount].firstmeasure+i].bpm *(NOTES_AREA * fabs(Measure[Measure[MeasureCount].firstmeasure+i].scroll)));
 							percent=(double)Measure[Measure[MeasureCount].firstmeasure+i].notes_count/(double)Measure[Measure[MeasureCount].firstmeasure].max_notes;
 
 							Measure[Measure[MeasureCount].firstmeasure+i].isDispBarLine=false;	//最初の小節は小節線をオフにしない
