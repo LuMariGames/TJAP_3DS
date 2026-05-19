512e1f570d93ea4cbf632f1ae1f3fb7beeb4b03a
LuMariGames <mariogames0120@gmail.com>
2026-05-19 19:17:01 +0900

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -444,7 +444,7 @@ int main(){
 		if (loadend<3)aptSetHomeAllowed(false);
 		else aptSetHomeAllowed(true);
 
-		usleep(8333-(uint64_t)(get_current_time(3)*1000000)%8333);
+		if(Option.dispFps)usleep(8333-(uint64_t)(get_current_time(3)*1000000)%8333);
 		C3D_FrameBegin(!Option.dispFps);
 
 		//上画面
