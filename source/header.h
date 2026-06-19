31e3199865acd5711ca468c8937dbfd46789a60a
LuMariGames <mariogames0120@gmail.com>
2026-06-19 15:22:40 +0900

    Update header.h

--- source/header.h
+++ source/header.h
@@ -15,7 +15,7 @@
 #include <algorithm>
 
 #define C3D_CMDBUF_SIZE 0x40000
-#define VERSION "2.2.3"
+#define VERSION "2.3.0-pre"
 #define DECODE_MEM 393216
 #define DEFAULT_DIR "sdmc:/tjafiles"
 #define NOTDEF_DIR "romfs:/tjafiles/"
