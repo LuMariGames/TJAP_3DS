1466425589d4ff8f9866629afe1c983c3b742b7f
LuMariGames <mariogames0120@gmail.com>
2026-07-20 08:32:26 +0900

    Update tja.cpp

--- source/tja.cpp
+++ source/tja.cpp
@@ -1009,7 +1009,7 @@ void load_tja_notes(int course,LIST_T Song){
 				Measure[i].create_time=Measure[i].judge_time;
 				break;
 			case COMMAND_NEXTSONG:
-				Measure[i].create_time=Measure[i].pop_time-3.006;
+				Measure[i].create_time=Measure[i].pop_time-1.503;
 				break;
 			case COMMAND_JPOSSCROLL:
 				Measure[i].create_time=Measure[i].pop_time;
