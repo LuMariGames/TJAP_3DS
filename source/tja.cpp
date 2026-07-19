2cbb0090c234b89816a13fdc9909d4ec91a8fad4
LuMariGames <mariogames0120@gmail.com>
2026-07-19 15:43:59 +0900

    Update tja.cpp

--- source/tja.cpp
+++ source/tja.cpp
@@ -810,7 +810,7 @@ void load_tja_notes(int course,LIST_T Song){
 						yscroll=Command.val[1];
 						break;
 					case COMMAND_NEXTSONG:
-						PreJudge+=2.505;
+						PreJudge+=3.006;
 						break;
 					case COMMAND_DELAY:
 						delay=Command.val[0];
