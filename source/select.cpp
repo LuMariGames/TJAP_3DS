ba48695b2707ec3f8b7c4d124d9f475ddf9c915a
LuMariGames <mariogames0120@gmail.com>
2026-08-12 13:23:35 +0900

    Update select.cpp

--- source/select.cpp
+++ source/select.cpp
@@ -82,54 +82,47 @@ void load_genre_file(int id) {
 	json_decref(json);
 }
 
-inline void load_file_list(const char* path) {
-
-	DIR* dir;
+inline void load_file_list(const char* path){
+	DIR* dir=opendir(path);
+	if(!dir)return;
 	struct dirent* dp;
+	char filename[512];
 
-	if ((dir=opendir(path))!=NULL) {
-
-		char filename[512];
-		while ((dp=readdir(dir))!=NULL) {
-
-			strlcpy(filename,path,sizeof(filename));
-			strcat(filename,"/");
-			strcat(filename,dp->d_name);
-
-			DIR* db=opendir(filename);
+	while((dp=readdir(dir))!= NULL){
+		if(strcmp(dp->d_name,".")==0||strcmp(dp->d_name,"..")==0){
+			continue;
+		}
+		snprintf(filename,sizeof(filename),"%s/%s",path,dp->d_name);
+		bool is_dir=false;
+		if(dp->d_type==DT_DIR){
+			is_dir=true;
+		} else if(dp->d_type=-DT_UNKNOWN){
 			struct stat st;
-			stat(filename,&st);
-
-			if ((st.st_mode & S_IFMT)!=S_IFDIR) {
-
-				if (db==NULL) {
-
-					if (strstr(dp->d_name,".tja")!=NULL) {
-
-						strlcpy(List[SongCount].tja,dp->d_name,sizeof(List[0].tja));
-						strlcpy(List[SongCount].path,path,sizeof(List[0].path));
-						List[SongCount].genre=GENRE_MAX+1;
-						if(!load_tja_head_simple(&List[SongCount])){
-							conv_tja(List[SongCount]);
-							load_tja_head_simple(&List[SongCount]);
-						}
-						loadend=1;
-						++SongCount;
-					}
-					if (strstr(dp->d_name,GENRE_FILE)!=NULL) {
-
-						strlcpy(Genre[GenreCount].path,path,sizeof(Genre[0].path));
-						load_genre_file(GenreCount);
-						++GenreCount;
-						set_genres();
-						SongNumber=SongCount;
-					}
+			if(stat(filename,&st)==0&&S_ISDIR(st.st_mode)){
+				is_dir=true;
+			}
+		}
+		if(is_dir){
+			load_file_list(filename);
+		} else {
+			if(strstr(dp->d_name,".tja\0")!= NULL){
+				strlcpy(List[SongCount].tja,dp->d_name,sizeof(List[0].tja));
+				strlcpy(List[SongCount].path,path,sizeof(List[0].path));
+				List[SongCount].genre=GENRE_MAX + 1;
+				if(!load_tja_head_simple(&List[SongCount])){
+					conv_tja(List[SongCount]);
+					load_tja_head_simple(&List[SongCount]);
 				}
+				loadend=1;
+				++SongCount;
 			}
-			else {
-				load_file_list(filename);
+			if(strstr(dp->d_name,GENRE_FILE)!= NULL){
+				strlcpy(Genre[GenreCount].path,path,sizeof(Genre[0].path));
+				load_genre_file(GenreCount);
+				++GenreCount;
+				set_genres();
+				SongNumber=SongCount;
 			}
-			closedir(db);
 		}
 	}
 	closedir(dir);
