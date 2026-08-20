78bd510fc9f78203e70d9abe81d813e568cd44e6
LuMariGames <mariogames0120@gmail.com>
2026-08-20 10:16:50 +0900

    Update notes.cpp

--- source/notes.cpp
+++ source/notes.cpp
@@ -61,7 +61,7 @@ void notes_main(int isDon,int isKatsu,char (&tja_notes)[MEASURE_MAX][NOTES_MEASU
 	bool isLoadLoop=false;
 
 	//最初の小節のcreate_timeがマイナスだった時用に調整
-	double CurrentTimeNotes=OffSetTime=Measure[stme].create_time+offset;
+	double CurrentTimeNotes=OffSetTime=Measure[stme].create_time-offset;
 	if(cnt>=0)CurrentTimeNotes=get_current_time(TIME_NOTES)-OffSetTime;
 	if(cnt==0)Branch.course=Measure[stme].branch;
 	//snprintf(get_buffer(),BUFFER_SIZE,"fmt:%.4f ctm:%.2f ct:%.2f 0ct:%.4f",get_FirstMeasureTime(),CurrentTimeNotes,CurrentTimeNotes-Measure[0].create_time,Measure[stme].create_time);
