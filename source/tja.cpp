e9eb0d7582cf8efefce4aa9772b50b5d1fb46710
LuMariGames <mariogames0120@gmail.com>
2026-08-27 16:03:02 +0900

    Update tja.cpp

--- source/tja.cpp
+++ source/tja.cpp
@@ -788,7 +788,9 @@ double calc_first_measure_time(){	//最初に到達する小節の所要時間�
 			break;
 		}
 	}
-	return Measure[stme].judge_time-Measure[stme].create_time;
+	if(fabs(Measure[stme].judge_time-Measure[stme].create_time)<2.00)
+		return Measure[stme].judge_time-Measure[stme].create_time;
+	else return Measure[stme].judge_time-Measure[0].create_time;
 }
 
 void load_tja_notes(int course,LIST_T Song){
