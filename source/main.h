93d332755b1400196c57a23f5422995c80340659
LuMariGames <mariogames0120@gmail.com>
2026-08-31 07:34:49 +0900

    Update main.h

--- source/main.h
+++ source/main.h
@@ -1,7 +1,7 @@
 ﻿#pragma once
 
 void draw_debug(float x, float y, const char *text),set_isPause(),debug_touch(int x, int y),play_songs(char* ptr);
-bool get_isPause(),get_isMusicStart();
+bool get_isPause(),get_isMusicStart(),isSpecial();
 char* get_buffer();
 int powi(int x, int y),pause_window(touchPosition tp, unsigned int key),
 message_window(touchPosition tp, unsigned int key,int text),exist_file(const char* path);
