9960511fa4555b62bc1fb2b43a8fd94a8c5df35d
LuMariGames <mariogames0120@gmail.com>
2026-08-30 14:31:03 +0900

    Update result.cpp

--- source/result.cpp
+++ source/result.cpp
@@ -15,11 +15,9 @@ void draw_result() {
 	
 	float size = 0.7f,width,height;
 
-	draw_result_text(50,10,0.6f,Tja.title,0xffffff,true);
-	draw_result_text(50,30,0.48f,Tja.subtitle,0xffffff,true);
-	if (Tja.course<6) draw_result_text(0,0,1.f,"");
-	else if (result.dcon < 0) draw_result_text(50,48,0.8f,"合格",0xfad132);
-	else if (result.dcon == 0) draw_result_text(50,48,0.8f,"合格",0xfd3104);
+	if(Tja.course<6)draw_result_text(0,0,1.f,"");
+	else if(result.dcon<0)draw_result_text(50,48,0.8f,"合格",0xfad132);
+	else if(result.dcon==0)draw_result_text(50,48,0.8f,"合格",0xfd3104);
 	else draw_result_text(50,48,0.8f,"不合格",0x02a6fd);
 
 	int iniX=100,iniY = 100,x2 = 300,YSense = 20,count = 0;
