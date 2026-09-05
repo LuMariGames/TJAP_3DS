a372fdb1c008918b4ee8668c94c9ff192f24b4fa
LuMariGames <mariogames0120@gmail.com>
2026-09-05 13:40:10 +0900

    Update tja.cpp

--- source/tja.cpp
+++ source/tja.cpp
@@ -390,7 +390,11 @@ bool load_tja_head(int course,LIST_T Song){
 						isCourseMatch=true;
 					}
 					else if(Current_Header.course==COURSE_ENDLESSDAN&&course<=COURSE_EDIT){
-						for(int n=4;n<0;--n){if(Song.course[n]){if(course==n){isCourseMatch=true;}else{isCourseMatch=false;}n=0;}}
+						if(Song.course[COURSE_EDIT]&&CurrentCourse==COURSE_EDIT)isCourseMatch=true;
+						else if(Song.course[COURSE_ONI]&&CurrentCourse==COURSE_ONI)isCourseMatch=true;
+						else if(Song.course[COURSE_HARD]&&CurrentCourse==COURSE_HARD)isCourseMatch=true;
+						else if(Song.course[COURSE_NORMAL]&&CurrentCourse==COURSE_NORMAL)isCourseMatch=true;
+						else if(Song.course[COURSE_EASY]&&CurrentCourse==COURSE_EASY)isCourseMatch=true;
 					}
 					else isCourseMatch=false;
 				}
@@ -879,7 +883,11 @@ void load_tja_notes(int course,LIST_T Song){
 				free(temp);
 				if(course!=COURSE_ENDLESSDAN&&course==CurrentCourse)isCourseMatch=true;
 				else if(course==COURSE_ENDLESSDAN&&CurrentCourse<=COURSE_EDIT){
-					for(int n=4;n<0;--n){if(Song.course[n]){if(CurrentCourse==n){isCourseMatch=true;}else{isCourseMatch=false;}n=0;}}
+					if(Song.course[COURSE_EDIT]&&CurrentCourse==COURSE_EDIT)isCourseMatch=true;
+					else if(Song.course[COURSE_ONI]&&CurrentCourse==COURSE_ONI)isCourseMatch=true;
+					else if(Song.course[COURSE_HARD]&&CurrentCourse==COURSE_HARD)isCourseMatch=true;
+					else if(Song.course[COURSE_NORMAL]&&CurrentCourse==COURSE_NORMAL)isCourseMatch=true;
+					else if(Song.course[COURSE_EASY]&&CurrentCourse==COURSE_EASY)isCourseMatch=true;
 				}
 				continue;
 			}
