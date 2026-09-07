90e7be0356fd15e401d4babe7d747619e322e2fc
LuMariGames <mariogames0120@gmail.com>
2026-09-07 17:34:54 +0900

    Update notes.cpp

--- source/notes.cpp
+++ source/notes.cpp
@@ -76,7 +76,7 @@ void notes_main(int isDon,int isKatsu,char (&tja_notes)[MEASURE_MAX][NOTES_MEASU
 			Branch.next=false;
 		}
 
-		while((Measure[MeasureCount].create_time<=CurrentTimeNotes||isHBSCROLL)&&!Branch.wait){
+		while(!Branch.wait){
 
 			NotesCount=0;
 			if(Measure[MeasureCount].branch!=Branch.course&&Measure[MeasureCount].branch!=-1){
