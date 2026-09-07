49431ec020c22f7b97b649f9964c3c71282308c0
LuMariGames <mariogames0120@gmail.com>
2026-09-07 17:55:36 +0900

    Update tja.cpp

--- source/tja.cpp
+++ source/tja.cpp
@@ -1156,12 +1156,12 @@ void load_tja_notes(int course,LIST_T Song){
 				Measure[i].judge_time=Measure[i].pop_time;
 				break;
 			case COMMAND_BRANCHSTART:
+			case COMMAND_BRANCHEND:
 				Measure[i].judge_time=Measure[i].pop_time-(240.0/(Measure[i].bpm*fabs(Measure[i].scroll)))*Measure[i].measure;
 				break;
 			case COMMAND_SECTION:
 			case COMMAND_GOGOSTART:
 			case COMMAND_GOGOEND:
-			case COMMAND_BRANCHEND:
 				Measure[i].judge_time=Measure[i].pop_time;
 				break;
 			}
