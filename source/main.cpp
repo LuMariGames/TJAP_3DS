1ea2cf87618c0187959fc68a1098ae9c95642261
LuMariGames <mariogames0120@gmail.com>
2026-09-10 09:30:50 +0900

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -533,7 +533,8 @@ int main(){
 		switch (scene_state){
 		case SCENE_SELECTLOAD:	//ロード画面
 
-			snprintf(get_buffer(),BUFFER_SIZE,"TJAPlayer for 3DS v%s",VERSION);
+			if(isEmu)snprintf(get_buffer(),BUFFER_SIZE,"TJAPlayer for 3DS(Emu) v%s",VERSION);
+			else snprintf(get_buffer(),BUFFER_SIZE,"TJAPlayer for 3DS v%s",VERSION);
 			draw_select_text(120,70,get_buffer());
 			draw_select_text(120,100,"Now Loading...");
 			if (loadend>0)draw_select_text(0,225,"SELECT to Skip ");
