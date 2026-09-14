9d3e52f1d67e885ea2e83a5efff8e85abb7dc294
LuMariGames <mariogames0120@gmail.com>
2026-09-15 08:01:52 +0900

    Update notes.cpp

--- source/notes.cpp
+++ source/notes.cpp
@@ -193,7 +193,7 @@ void notes_main(int isDon,int isKatsu,char (&tja_notes)[MEASURE_MAX][NOTES_MEASU
 
 					Notes[id].judge_time=Measure[MeasureCount].judge_time+NoteTime;
 					Notes[id].move_time=Measure[MeasureCount].move_time;
-					Notes[id].create_time=Measure[MeasureCount].create_time+NoteTime;
+					if(!isHBSCROLL)Notes[id].create_time=Measure[MeasureCount].create_time+NoteTime;
 					Notes[id].num=Measure[MeasureCount].notes;
 					Notes[id].scroll=Measure[MeasureCount].scroll*Option.speed;
 					Notes[id].yscroll=Measure[MeasureCount].yscroll*Option.speed;
