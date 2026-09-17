820d634f74a319c688c8fcec31fb4a30b9818f26
LuMariGames <mariogames0120@gmail.com>
2026-09-17 17:43:01 +0900

    Update score.cpp

--- source/score.cpp
+++ source/score.cpp
@@ -10,7 +10,7 @@ extern uint64_t Cdn[2][4];
 extern char *exam[4][4];
 bool isGOGO;
 uint64_t combo,init,diff,DiffMul,scoremode,HitScore,ScoreDiff,courselife,
-TotalPerfectCount,TotalNiceCount,TotalBadCount,TotalScore,TotalRollCount,
+TotalPerfectCount,TotalNiceCount,TotalBadCount,TotalScore,TotalRollCount,TotalBombCount,
 TotalCount,MaxComboCount,CurrentPerfectCount,CurrentNiceCount,CurrentBadCount,
 CurrentTotalRollCount,CurrentRollCount,CurrentScore,CurrentDaiNotes;
 int CurrentBalloonCount;
@@ -49,6 +49,7 @@ void init_score(){
 	TotalPerfectCount=0;
 	TotalNiceCount=0;
 	TotalBadCount=0;
+	TotalBombCount=0;
 	CurrentPerfectCount=0;
 	CurrentNiceCount=0;
 	CurrentBadCount=0;
@@ -262,6 +263,12 @@ void update_score(int knd){
 
 	case ROLL_END:
 		CurrentRollCount=0;
+		break;
+
+	case BOMB:
+		combo=0;
+		Gauge.score-=Gauge.bad;
+		++TotalBombCount;
 		break;
 	}
 	//}
@@ -504,10 +511,10 @@ void send_gogotime(bool arg){
 
 int dan_condition(){
 	int isBadCondition=-1;//ここで何回条件に反しているかをリセットする
-	const char* types[]={"jb","jg","jp","s","r","h","c","g"};
-	uint64_t counts[]={TotalBadCount,TotalNiceCount,TotalPerfectCount,TotalScore,TotalRollCount,TotalCount,MaxComboCount,(uint64_t)(Gauge.score/Gauge.soul*100.0)};
+	const char* types[]={"jb","jg","jp","s","r","h","c","g","jm"};
+	uint64_t counts[]={TotalBadCount,TotalNiceCount,TotalPerfectCount,TotalScore,TotalRollCount,TotalCount,MaxComboCount,(uint64_t)(Gauge.score/Gauge.soul*100.0),TotalBombCount};
 
-	for(int i=0;i<8;++i){
+	for(int i=0;i<9;++i){
 		if((strcmp(exam[0][0],types[i])==0&&strcmp(exam[0][3],"l")==0&&counts[i]>=Cdn[1][0])||
 		(strcmp(exam[1][0],types[i])==0&&strcmp(exam[1][3],"l")==0&&counts[i]>=Cdn[1][1])||
 		(strcmp(exam[2][0],types[i])==0&&strcmp(exam[2][3],"l")==0&&counts[i]>=Cdn[1][2])||
@@ -517,7 +524,7 @@ int dan_condition(){
 		}
 	}
 
-	for(int i=0;i<8;++i){
+	for(int i=0;i<9;++i){
 		if((strcmp(exam[0][0],types[i])==0&&strcmp(exam[0][3],"l")==0&&counts[i]>=Cdn[0][0])||
 		(strcmp(exam[1][0],types[i])==0&&strcmp(exam[1][3],"l")==0&&counts[i]>=Cdn[0][1])||
 		(strcmp(exam[2][0],types[i])==0&&strcmp(exam[2][3],"l")==0&&counts[i]>=Cdn[0][2])||
@@ -525,7 +532,7 @@ int dan_condition(){
 	}
 
 	if(get_notes_finish()==true){ //条件に以上(例:exam[3]="m")がある場合、曲が終わるまで判定しない
-		for(int i=0;i<8;++i){
+		for(int i=0;i<9;++i){
 			if(isBadCondition==-1 &&
 			((strcmp(exam[0][0],types[i])==0&&strcmp(exam[0][3],"m")==0&&counts[i]<Cdn[1][0])||
 			(strcmp(exam[1][0],types[i])==0&&strcmp(exam[1][3],"m")==0&&counts[i]<Cdn[1][1])||
@@ -535,7 +542,7 @@ int dan_condition(){
 				i=7;
 			}
 		}
-		for(int i=0;i<8;++i){
+		for(int i=0;i<9;++i){
 			if((strcmp(exam[0][0],types[i])==0&&strcmp(exam[0][3],"m")==0&&counts[i]<Cdn[0][0])||
 			(strcmp(exam[1][0],types[i])==0&&strcmp(exam[1][3],"m")==0&&counts[i]<Cdn[0][1])||
 			(strcmp(exam[2][0],types[i])==0&&strcmp(exam[2][3],"m")==0&&counts[i]<Cdn[0][2])||
