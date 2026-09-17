2896e573e6602d5e4a633ce73b7a45546a7ab920
LuMariGames <mariogames0120@gmail.com>
2026-09-17 19:14:15 +0900

    Update score.cpp

--- source/score.cpp
+++ source/score.cpp
@@ -520,7 +520,7 @@ int dan_condition(){
 		(strcmp(exam[2][0],types[i])==0&&strcmp(exam[2][3],"l")==0&&counts[i]>=Cdn[1][2])||
 		(strcmp(exam[3][0],types[i])==0&&strcmp(exam[3][3],"l")==0&&counts[i]>=Cdn[1][3])){
 			isBadCondition=0;
-			i=7;
+			i=99;
 		}
 	}
 
@@ -539,7 +539,7 @@ int dan_condition(){
 			(strcmp(exam[2][0],types[i])==0&&strcmp(exam[2][3],"m")==0&&counts[i]<Cdn[1][2])||
 			(strcmp(exam[3][0],types[i])==0&&strcmp(exam[3][3],"m")==0&&counts[i]<Cdn[1][3]))){
 				isBadCondition=0;
-				i=7;
+				i=99;
 			}
 		}
 		for(int i=0;i<9;++i){
