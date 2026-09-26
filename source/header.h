f30667e6ba888dab8555dc24ffbb1539bbbf9513
LuMariGames <mariogames0120@gmail.com>
2026-09-26 09:20:57 +0900

    Update header.h

--- source/header.h
+++ source/header.h
@@ -19,7 +19,7 @@
 #define VERSION "2.4.0(B)-pre"
 #define DECODE_MEM 393216
 #define DEFAULT_DIR "sdmc:/tjafiles"
-#define NOTDEF_DIR "romfs:/tjafiles/"
+#define NOTDEF_DIR "romfs:/tjafiles"
 #define SETTING_FILE "sdmc:/TJAPlayerfor3ds_setting.json"
 #define SKIN_SETTING_FILE "sdmc:/tjafiles/theme/config.json"
 #define PATH_DSP1 "sdmc:/3ds/dspfirm.cdc"
